3ab9698720c033931ca8f3d04a9b8d1ad49665b3
Daisuke Tsukada <dsgarage@gmail.com>
2020-12-11 18:26:22 +0900

    マイフレーム１が足されて、UIを更新

--- Assets/UniRxOperaterTest.cs
+++ Assets/UniRxOperaterTest.cs
@@ -2,9 +2,13 @@
 using System.Collections.Generic;
 using UniRx;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UniRxOperaterTest : MonoBehaviour
 {
+    [SerializeField] private Text everyUpdateText;
+    [SerializeField] private int valu = 0;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -14,7 +18,11 @@ public class UniRxOperaterTest : MonoBehaviour
         
         ///マイフレーム呼び出してくれる
         var every = Observable.EveryUpdate();
-        every.Subscribe( _ => Debug.Log( "Hello! EveryUpDate." ) );
+        every.Subscribe(_ =>
+        {
+            valu += 1;
+            everyUpdateText.text = valu.ToString();
+        });
     }
 
     // Update is called once per frame
