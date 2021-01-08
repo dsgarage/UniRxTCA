3a26f1950e386c9522849d6d1275868dcf5adff5
Daisuke Tsukada <dsgarage@gmail.com>
2021-01-08 18:13:31 +0900

    ReactiveCollectionTest

---
+++ Assets/UniRxReactiveCollectionTest.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UniRx;
+using UnityEngine;
+
+public class UniRxReactiveCollectionTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        var collection = new ReactiveCollection<string>();
+
+        collection.ObserveAdd().Subscribe(x => Debug.Log(string.Format("Add [{0}] = {1}", x.Index, x.Value)));
+        collection.ObserveRemove().Subscribe(x => Debug.Log(string.Format("Remove [{0}] = {1}", x.Index, x.Value)));
+        
+        collection.Add("Apple");
+        collection.Add("Baseball");
+        collection.Add("Cherry");
+        collection.Remove("Apple");
+    }
+}
