4fd7f3c311b472ee538b1acd4a5d0abef917071c
Daisuke Tsukada <dsgarage@gmail.com>
2021-01-08 17:39:55 +0900

    AsyncSubjectA&B

---
+++ Assets/AsyncSubjectSampleA.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UniRx;
+using UnityEngine;
+
+public class AsyncSubjectSampleA : MonoBehaviour
+{
+    AsyncSubject<Unit> initializeAsyncSubject = new AsyncSubject<Unit>();
+
+    public IObservable<Unit> OninitializedAsync
+    {
+        get { return initializeAsyncSubject; }
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("＝＝SampleAの初期化が実行されました＝＝");
+
+        //初期化処理
+        
+        Debug.Log("＝＝SampleAの初期化が終わりました＝＝");
+
+        initializeAsyncSubject.OnNext(Unit.Default);
+        Debug.Log("＝＝＝＝通知しました＝＝＝＝");
+        initializeAsyncSubject.OnCompleted();
+        
+    }
+}
---
+++ Assets/AsyncSubjectSampleB.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using UniRx;
+using UnityEngine;
+
+public class AsyncSubjectSampleB : MonoBehaviour
+{
+    [SerializeField] private AsyncSubjectSampleA sampleA;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("＝＝SampleBの初期化が実行されました＝＝");
+        sampleA.OninitializedAsync.Subscribe(x => Initialize());
+    }
+
+    void Initialize()
+    {
+        Debug.Log("＝＝SampleBの初期化が終わりました＝＝");
+    }
+
+
+}
