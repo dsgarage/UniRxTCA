27c09d82098c5d8b11c2d8f2edd60aba7ec8e7dc
Daisuke Tsukada <dsgarage@gmail.com>
2021-01-08 17:15:18 +0900

    BehabiourSubjectサンプル

---
+++ Assets/UniRxBehaviorSubjectTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UniRx;
+using UnityEngine;
+
+public class UniRxBehaviorSubjectTest : MonoBehaviour
+{
+    BehaviorSubject<int> subject = new BehaviorSubject<int>(5);
+
+    private void Start()
+    {
+        subject.Subscribe(x => Debug.Log(x));
+        StartCoroutine(SetOnNext());
+    }
+
+    IEnumerator SetOnNext()
+    {
+        subject.OnNext(1);
+        
+        yield return new WaitForSeconds(1);
+        
+        subject.OnNext(2);
+
+        yield return new WaitForSeconds(1);
+        
+        subject.OnNext(3);
+        
+    }
+}
