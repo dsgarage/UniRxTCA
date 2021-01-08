ecedabc06e7ca2734ee0d8b95f5dd7e311efe4c9
Daisuke Tsukada <dsgarage@gmail.com>
2021-01-08 18:37:29 +0900

    FactryMethod Create

---
+++ Assets/UniRxFactryMethodCreate.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UniRx;
+using UnityEngine;
+
+public class UniRxFactryMethodCreate : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Observable.Create<int>(observer =>
+        {
+            Debug.Log("Start");
+
+            for (var i = 0; i <= 100; i += 10)
+            {
+                observer.OnNext(i);
+            }
+
+            Debug.Log("Finished");
+
+            observer.OnCompleted();
+
+            return Disposable.Create(() => { Debug.Log("Despose"); });
+        }).Subscribe(x => Debug.Log(x));
+    }
+
+}
