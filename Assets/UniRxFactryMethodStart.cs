1cdc4e75423e9d0fa4cdd5cba8f5ff77e3784cc2
Daisuke Tsukada <dsgarage@gmail.com>
2021-01-08 19:09:58 +0900

    FactryMethod Start

---
+++ Assets/UniRxFactryMethodStart.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using UniRx;
+using UnityEngine;
+
+public class UniRxFactryMethodStart : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Observable.Start(() =>
+            {
+                //GoogleのTOPページをHTTPでGETする
+                var req = (HttpWebRequest) WebRequest.Create("http://google.com");
+                var res = (HttpWebResponse) req.GetResponse();
+                using (var reader = new StreamReader(res.GetResponseStream()))
+                {
+                    return reader.ReadToEnd();
+                }
+            })
+            .ObserveOnMainThread()
+            .Subscribe(x => Debug.Log(x));
+    }
+
+}
