54c7b20c7d772837aa1cd651b499fbc7689abb0b
Daisuke Tsukada <dsgarage@gmail.com>
2021-01-22 18:58:22 +0900

    Select ＆　Cast

---
+++ Assets/UniRxOperatorCast.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UniRx;
+using UnityEngine;
+
+public class UniRxOperatorCast : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        var objects = new object[]
+        {
+            "hoge",
+            "fuga",
+            'a',
+            -1,
+            "fuga",
+            'z',
+            0.1
+        };
+
+        objects.ToObservable().Cast<object, string>().Subscribe(
+            x => Debug.Log(x),
+            ex => Debug.LogError(ex),
+            () => Debug.Log("OnCompleted"));
+    }  
+    
+}
---
+++ Assets/UniRxOperatorSelect.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UniRx;
+using UnityEngine;
+
+public class UniRxOperatorSelect : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Observable.Range(1, 5).Select(x => x * 10).Subscribe(x => Debug.Log(x));
+    }
+}
