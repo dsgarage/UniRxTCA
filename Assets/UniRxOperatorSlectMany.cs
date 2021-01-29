7334eee152904beb662c4a3dda0d4734fb8b10fb
Daisuke Tsukada <dsgarage@gmail.com>
2021-01-29 17:41:38 +0900

    ファイル整理

---
+++ Assets/UniRxOperatorSlectMany.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Cysharp.Threading.Tasks;
+using UniRx;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+
+public class UniRxOperatorSlectMany : MonoBehaviour
+{
+    [SerializeField] private Button _downloadButton;
+    [SerializeField] private InputField _urlInputField;
+
+
+    private string[] urls = new[]
+    {
+        "https://unity3d.com/jp/",
+        "https://www.google.co.jp",
+        "https://www.bing.com"
+    };
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _downloadButton.OnClickAsObservable().Select(_ => _urlInputField.text)
+            .SelectMany(url => FetchAsync(url).ToObservable()).Subscribe(x => Debug.Log(x));
+
+
+        urls.ToObservable().SelectMany(uri => TryGetAsync(uri).ToObservable(), (uri, body) => (uri, body)).
+            Subscribe(x =>
+            {
+                var (uri, body) = x;
+                Debug.Log($"{uri}への通信結果:{body}");
+            });
+
+
+    }
+    
+    private async UniTask<string> FetchAsync(string url)
+    {
+        using (var uwr = UnityWebRequest.Get(url))
+        {
+            await uwr.SendWebRequest();
+            return uwr.downloadHandler.text;
+        }
+    }
+
+    private async UniTask<bool> TryGetAsync(string url)
+    {
+        using (var uwr = UnityWebRequest.Get(url))
+        {
+            try
+            {
+                await uwr.SendWebRequest();
+                return true;
+            }
+            catch (Exception e)
+            {
+                Debug.Log(e);
+                return false;
+            }
+        }
+    }
+}
---
+++ Assets/UniRxSample/UniRxOperatorMarge.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UniRx;
+using UnityEngine;
+
+public class UniRxOperatorMarge : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //var s1 = Observable.Range(10, 3, Scheduler.MainThread);
+        //var s2 = Observable.Range(20, 3, Scheduler.MainThread);
+
+        //s1.Merge(s2).Subscribe(x => Debug.Log($"{Time.frameCount} - {x}"));
+
+        //IObservable<IObservable<int>> streams 
+        //    = Observable.Range(1, 3, Scheduler.Immediate).Select(x =>
+        //{
+        //    return Observable.Range(x * 100, 3, Scheduler.Immediate);
+        //});
+
+        IEnumerable<IObservable<int>> streams = new[]
+        {
+            Observable.Range(100, 3),
+            Observable.Range(200, 3),
+            Observable.Range(300, 3)
+        };
+
+        streams.Merge().Subscribe(x => Debug.Log(x));
+    }
+
+}
