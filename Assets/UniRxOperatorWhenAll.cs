b42215eb676bda6e12b5998bf3bf94c59a2cd1a1
Daisuke Tsukada <dsgarage@gmail.com>
2021-02-05 17:19:23 +0900

    WhenAll＆WithLatestFrom

---
+++ Assets/UniRxOperatorWhenAll.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Cysharp.Threading.Tasks;
+using UniRx;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class UniRxOperatorWhenAll : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        var parallel = Observable.WhenAll(
+            FetchAsync("https://unity.com/ja").ToObservable(),
+            FetchAsync("https://www.google.co.jp").ToObservable(),
+            FetchAsync("https://github.com").ToObservable());
+        parallel.Subscribe(xs =>
+        {
+            Debug.Log(xs[0]);//Unity
+            Debug.Log(xs[1]);//Google
+            Debug.Log(xs[2]);//GitHub
+        });
+    }
+
+
+    async UniTask<string> FetchAsync(string uri)
+    {
+        using (var uwr = UnityWebRequest.Get(uri))
+        {
+            await uwr.SendWebRequest();
+            return uwr.downloadHandler.text;
+        }
+    }
+}
---
+++ Assets/UniRxOperatorWithLatestFrom.cs
@@ -0,0 +1,30 @@
+﻿using UniRx;
+using UniRx.Triggers;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class UniRxOperatorWithLatestFrom : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        var rigidbody = GetComponent<Rigidbody>();
+        var inputStream = this.UpdateAsObservable().Select(_ =>
+        {
+            Debug.Log("Call InputStream.");
+            return new Vector3(
+                x: Input.GetAxis("Horizontal"),
+                y: 0,
+                z: Input.GetAxis("Vertical"));
+        });
+
+        this.FixedUpdateAsObservable()
+            .WithLatestFrom(inputStream, (_, input) => input)
+            .Subscribe(input =>
+        {
+            Debug.Log("Call FixdUpdate.");
+            rigidbody.AddForce(input, ForceMode.Acceleration);
+        });
+    }
+
+}
